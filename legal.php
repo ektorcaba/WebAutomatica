7a5b6e2618fce96e1067d66b4ae26c37ab81e659
ektorcaba <ektorcaba@gmail.com>
2023-08-27 11:16:06 +0100

    Update legal.php
    Texto legal

--- legal.php
+++ legal.php
@@ -1,7 +1,109 @@
 <?php require "header.php"; ?>
 <main>
-<div class="cover">
-En calidad de Afiliado de Amazon, obtengo ingresos por las compras adscritas que cumplen los requisitos aplicables según las <a href="https://afiliados.amazon.es/help/operating/agreement#Schedule1">condiciones establecidas por Amazon para afiliados</a>. Los servicios y/o productos están disponibles sólo para las personas jurídicas y las personas que tengan 16 años.
-</div>
+<p>En calidad de Afiliado de Amazon, obtengo ingresos por las compras adscritas que cumplen los requisitos aplicables según las <a href="https://afiliados.amazon.es/help/operating/agreement#Schedule1">condiciones establecidas por Amazon para afiliados</a>. Los servicios y/o productos están disponibles sólo para las personas jurídicas y las personas que tengan 16 años.</p>
+
+<h2>Identificaci&oacute;n y Titularidad</h2>
+
+<p>En cumplimiento del art&iacute;culo 10 de la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Informaci&oacute;n y Comercio Electr&oacute;nico, el Titular expone sus datos identificativos:</p>
+
+<ul>
+	<li><strong>Titular:</strong>&nbsp; Hector Cabrera.</li>
+	<li><strong>Domicilio:</strong>&nbsp; - Espa&ntilde;a.</li>
+	<li><strong>Sitio Web:</strong>&nbsp;</li>
+</ul>
+
+<h2>Finalidad</h2>
+
+<p>La finalidad del Sitio Web es: Venta.</p>
+
+<h2>Condiciones de Uso</h2>
+
+<p>La utilizaci&oacute;n del Sitio Web le otorga la condici&oacute;n de Usuario, e implica la aceptaci&oacute;n completa de todas las cl&aacute;usulas y condiciones de uso incluidas.</p>
+
+<p>Si no estuviera conforme con todas y cada una de estas cl&aacute;usulas y condiciones abst&eacute;ngase de utilizar el Sitio Web.</p>
+
+<p>El acceso al Sitio Web no supone, en modo alguno, el inicio de una relaci&oacute;n comercial con el Titular.</p>
+
+<p>A trav&eacute;s del Sitio Web, el Titular le facilita el acceso y la utilizaci&oacute;n de diversos contenidos que el Titular y/o sus colaboradores han publicado por medio de Internet.</p>
+
+<p>A tal efecto, est&aacute; obligado y comprometido a NO utilizar cualquiera de los contenidos del Sitio Web con fines o efectos il&iacute;citos, prohibidos en este Aviso Legal o por la legislaci&oacute;n vigente, lesivos de los derechos e intereses de terceros, o que de cualquier forma puedan da&ntilde;ar, inutilizar, sobrecargar, deteriorar o impedir la normal utilizaci&oacute;n de los contenidos, los equipos inform&aacute;ticos o los documentos, archivos y toda clase de contenidos almacenados en cualquier equipo inform&aacute;tico propios o contratados por el Titular, de otros usuarios o de cualquier usuario de Internet.</p>
+
+<h2>Mayor&iacute;a de edad</h2>
+
+<p>Debe tener al menos 18 a&ntilde;os de edad para usar los servicios ofrecidos por el Titular o la mayor&iacute;a de edad que se requiera su pa&iacute;s para registrarse en el Sitio Web o utilizarlo. Si reside en un pa&iacute;s que no pertenezca a la Uni&oacute;n Europea, debe tener al menos 16 a&ntilde;os de edad para usar el sitio Web o la mayor&iacute;a de edad que se requiera su pa&iacute;s para registrarse en el sitio Web o utilizarlo.</p>
+
+<p>Adem&aacute;s de tener la edad m&iacute;nima requerida para usar el sitio Web en virtud de la ley aplicable, si no tiene la edad suficiente para poder las condiciones en su pa&iacute;s, su padre, madre, o tutor deben aceptar las condiciones en su nombre.</p>
+
+<h2>Medidas de seguridad</h2>
+
+<p>Los datos personales que facilite al Titular pueden ser almacenados en bases de datos automatizadas o no, cuya titularidad corresponde en exclusiva al Titular, que asume todas las medidas de &iacute;ndole t&eacute;cnica, organizativa y de seguridad que garantizan la confidencialidad, integridad y calidad de la informaci&oacute;n contenida en las mismas de acuerdo con lo establecido en la normativa vigente en protecci&oacute;n de datos.</p>
+
+<p>No obstante, debe ser consciente de que las medidas de seguridad de los sistemas inform&aacute;ticos en Internet no son enteramente fiables y que, por tanto el Titular no puede garantizar la inexistencia de virus u otros elementos que puedan producir alteraciones en los sistemas inform&aacute;ticos (software y hardware) del Usuario o en sus documentos electr&oacute;nicos y ficheros contenidos en los mismos aunque el Titular pone todos los medios necesarios y toma las medidas de seguridad oportunas para evitar la presencia de estos elementos da&ntilde;inos.</p>
+
+<h2>Tratamiento de Datos Personales</h2>
+
+<p>Puede consultar toda la informaci&oacute;n relativa al tratamiento de datos personales que recoge el Titular en la p&aacute;gina de&nbsp;Pol&iacute;tica de Privacidad.</p>
+
+<h2>Contenidos</h2>
+
+<p>El Titular ha obtenido la informaci&oacute;n, el contenido multimedia y los materiales incluidos en el Sitio Web de fuentes que considera fiables, pero, si bien ha tomado todas las medidas razonables para asegurar que la informaci&oacute;n contenida es correcta, el Titular no garantiza que sea exacta, completa o actualizada. El Titular declina expresamente cualquier responsabilidad por error u omisi&oacute;n en la informaci&oacute;n contenida en las p&aacute;ginas del Sitio Web.</p>
+
+<p>Queda prohibido transmitir o enviar a trav&eacute;s del Sitio Web cualquier contenido ilegal o il&iacute;cito, virus inform&aacute;ticos, o mensajes que, en general, afecten o violen derechos del Titular o de terceros.</p>
+
+<p>Los contenidos del Sitio Web tienen &uacute;nicamente una finalidad informativa y bajo ninguna circunstancia deben usarse ni considerarse como oferta de venta, solicitud de una oferta de compra ni recomendaci&oacute;n para realizar cualquier otra operaci&oacute;n, salvo que as&iacute; se indique expresamente.</p>
+
+<p>El Titular se reserva el derecho a modificar, suspender, cancelar o restringir el contenido del Sitio Web, los v&iacute;nculos o la informaci&oacute;n obtenida a trav&eacute;s del Sitio Web, sin necesidad de previo aviso.</p>
+
+<p>El Titular&nbsp;no es responsable de los da&ntilde;os y perjuicios que pudieran derivarse de la utilizaci&oacute;n de la informaci&oacute;n del Sitio Web o de la contenida en las redes sociales del Titular.</p>
+
+<h2>Pol&iacute;tica de cookies</h2>
+
+<p>Puede consultar toda la informaci&oacute;n relativa a la pol&iacute;tica de recogida y tratamiento de las cookies en la p&aacute;gina de&nbsp;Pol&iacute;tica de Cookies.</p>
+
+<h2>Enlaces a otros sitios Web</h2>
+
+<p>El Titular puede proporcionarle acceso a sitios Web de terceros mediante enlaces con la finalidad exclusiva de informar sobre la existencia de otras fuentes de informaci&oacute;n en Internet en las que podr&aacute; ampliar los datos ofrecidos en el Sitio Web.</p>
+
+<p>Estos enlaces a otros sitios Web no suponen en ning&uacute;n caso una sugerencia o recomendaci&oacute;n para que usted visite las p&aacute;ginas web de destino, que est&aacute;n fuera del control del Titular, por lo que el Titular no es responsable del contenido de los sitios web vinculados ni del resultado que obtenga al seguir los enlaces. Asimismo, el Titular no responde de los links o enlaces ubicados en los sitios web vinculados a los que le proporciona acceso.</p>
+
+<p>El establecimiento del enlace no implica en ning&uacute;n caso la existencia de relaciones entre el Titular y el propietario del sitio en el que se establezca el enlace, ni la aceptaci&oacute;n o aprobaci&oacute;n por parte del Titular de sus contenidos o servicios.</p>
+
+<p>Si accede a un sitio web externo desde un enlace que encuentre en el Sitio Web usted deber&aacute; leer la propia pol&iacute;tica de privacidad del otro sitio web que puede ser diferente de la de este sitio Web.</p>
+
+<h2>Enlaces de Afiliados y anuncios patrocinados</h2>
+
+<p>El Sitio Web ofrece contenidos patrocinados, anuncios y/o enlaces de afiliados.</p>
+
+<p>La informaci&oacute;n que aparece en estos enlaces de afiliados o los anuncios insertados, son facilitados por los propios anunciantes, por lo que el Titular no se hace responsable de posibles inexactitudes o errores que pudieran contener los anuncios, ni garantiza en modo alguno la experiencia, integridad o responsabilidad de los anunciantes o la calidad de sus productos y/o servicios.</p>
+
+<h2>Aviso legal de Amazon Afiliados</h2>
+
+<p>En calidad de Afiliado de Amazon, obtengo ingresos por las compras adscritas que cumplen los requisitos aplicables.</p>
+
+<h2>Propiedad intelectual e industrial</h2>
+
+<p>Todos los derechos est&aacute;n reservados.</p>
+
+<p>Todo acceso a este Sitio Web est&aacute; sujeto a las siguientes condiciones: la reproducci&oacute;n, almacenaje permanente y la difusi&oacute;n de los contenidos o cualquier otro uso que tenga finalidad p&uacute;blica o comercial queda expresamente prohibida sin el consentimiento previo expreso y por escrito del Titular.</p>
+
+<h2>Limitaci&oacute;n de responsabilidad</h2>
+
+<p>La informaci&oacute;n y servicios incluidos o disponibles a trav&eacute;s del Sitio Web pueden incluir incorrecciones o errores tipogr&aacute;ficos. De forma peri&oacute;dica el Titular incorpora mejoras y/o cambios a la informaci&oacute;n contenida y/o los Servicios que puede introducir en cualquier momento.</p>
+
+<p>El Titular no declara ni garantiza que los servicios o contenidos sean interrumpidos o que est&eacute;n libres de errores, que los defectos sean corregidos, o que el servicio o el servidor que lo pone a disposici&oacute;n est&eacute;n libres de virus u otros componentes nocivos sin perjuicio de que el Titular realiza todos los esfuerzos en evitar este tipo de incidentes.</p>
+
+<p>El Titular declina cualquier responsabilidad en caso de que existan interrupciones o un mal funcionamiento de los Servicios o contenidos ofrecidos en Internet, cualquiera que sea su causa. Asimismo, el Titular no se hace responsable por ca&iacute;das de la red, p&eacute;rdidas de negocio a consecuencia de dichas ca&iacute;das, suspensiones temporales de fluido el&eacute;ctrico o cualquier otro tipo de da&ntilde;o indirecto que te pueda ser causado por causas ajenas a el Titular.</p>
+
+<p>Antes de tomar decisiones y/o acciones con base a la informaci&oacute;n incluida en el Sitio Web, el Titular le recomienda comprobar y contrastar la informaci&oacute;n recibida con otras fuentes.</p>
+
+<h2>Derecho de exclusi&oacute;n</h2>
+
+<p>El Titular se reserva el derecho a denegar o retirar el acceso al Sitio Web y los servicios ofrecidos sin necesidad de preaviso, a instancia propia o de un tercero, a aquellos usuarios que incumplan cualquiera de las condiciones de este Aviso Legal.</p>
+
+<h2>Jurisdicci&oacute;n</h2>
+
+<p>Este Aviso Legal se rige &iacute;ntegramente por la legislaci&oacute;n espa&ntilde;ola.</p>
+
+  
 </main>
 <?php require "footer.php"; ?>
