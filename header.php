6df83a3b51363b595d2558f6f9b2e547f9ebfaa6
ektorcaba <ektorcaba@gmail.com>
2023-08-27 11:37:52 +0100

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="index, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title><?php
 
