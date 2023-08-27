1815409020d7783f179b2e1247d3d020fc7a5c08
ektorcaba <ektorcaba@gmail.com>
2023-08-27 11:08:12 +0100

    Update legal.php

--- legal.php
+++ legal.php
@@ -1,1 +1,7 @@
+<?php require "header.php"; ?>
+<main>
+<div class="cover">
 En calidad de Afiliado de Amazon, obtengo ingresos por las compras adscritas que cumplen los requisitos aplicables según las <a href="https://afiliados.amazon.es/help/operating/agreement#Schedule1">condiciones establecidas por Amazon para afiliados</a>. Los servicios y/o productos están disponibles sólo para las personas jurídicas y las personas que tengan 16 años.
+</div>
+</main>
+<?php require "footer.php"; ?>
