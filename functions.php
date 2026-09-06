3171cfc1d4d2f116f2df8b0089be70f826cca881
ektorcaba <ektorcaba@gmail.com>
2026-09-06 12:21:29 +0100

    Update Retry-After header value to 432000 seconds

--- functions.php
+++ functions.php
@@ -430,7 +430,7 @@ if($keyword['indexed'] !== 1){
 	http_response_code(503);
 
 	// (Opcional) Indicar a los buscadores cuándo volver a intentarlo (ej. en 24 horas = 86400 segundos)
-	header('Retry-After: 259200');
+	header('Retry-After: 432000');
 
 	// (Opcional) Evitar que la respuesta vacía/temporal se quede cacheada
 	header('Cache-Control: no-cache, no-store, must-revalidate');
