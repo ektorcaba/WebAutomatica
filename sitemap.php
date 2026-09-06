ba94fc15fbf26daff790c98f85dc4fa5c8e97670
ektorcaba <ektorcaba@gmail.com>
2026-09-06 11:59:39 +0100

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -3,6 +3,12 @@
 require "inc/settings.php";
 
 
+// Consulta para obtener la fecha máxima formateada como YYYY-MM-DD
+    $stmt = $db->query("SELECT DATE_FORMAT(MAX(fecha), '%Y-%m-%d') AS ultima_fecha FROM keywords");
+    
+    // Obtener el valor directo
+    $fecha_lastmod = $stmt->fetchColumn();
+
 
 header('Content-type: text/xml');
 
@@ -12,12 +18,12 @@ header('Content-type: text/xml');
    .'<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">';
 
    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-   echo '<url><loc>'.$actual_link.'</loc></url>';
+   echo '<url><loc>'.$actual_link.'</loc><lastmod>'.$fecha_lastmod.'</lastmod></url>';
 
    foreach($db->query("SELECT * FROM menu")->fetchAll() as $link){
 
       //echo '<url><loc>'.$actual_link."/categoria/".str_replace(" ","-",strtolower($link['url'])).'</loc><lastmod>'.date('Y-m-d').'</lastmod><changefreq>daily</changefreq></url>';
-      echo '<url><loc>'.$actual_link."/categoria/".str_replace(" ","-",strtolower($link['url'])).'</loc></url>';
+      echo '<url><loc>'.$actual_link."/categoria/".str_replace(" ","-",strtolower($link['url'])).'</loc><lastmod>'.$fecha_lastmod.'</lastmod></url>';
 	   
    }
 
@@ -25,7 +31,7 @@ header('Content-type: text/xml');
    foreach($db->query("SELECT DISTINCT(slug) FROM keywords")->fetchAll() as $link){
    //foreach($db->query("SELECT DISTINCT(slug) FROM keywords WHERE indexed=1")->fetchAll() as $link){
       //echo '<url><loc>'.$actual_link.'/'.$link['slug'].'</loc><lastmod>'.date('Y-m-d').'</lastmod><changefreq>daily</changefreq></url>';
-      echo '<url><loc>'.$actual_link.'/'.$link['slug'].'</loc></url>';
+      echo '<url><loc>'.$actual_link.'/'.$link['slug'].'</loc><lastmod>'.$fecha_lastmod.'</lastmod></url>';
 
 
    }
